9626fe0e254815f7c30a157cfa9e7dc00bb08ea7
AbdKarim <41269340+Karimfactory@users.noreply.github.com>
2020-02-11 19:33:35 +0100

    Update Arrays.js

--- Arrays.js
+++ Arrays.js
@@ -1,6 +1,6 @@
 // création de tableaux
 
-let arr1 = [a, b ,c];
+let arr1 = ['a','b','c'];
 let arr2 = [{'nom':'Abdelhadi','prénom':'Karim'},
             {'nom':'John','prénom':'Wick'},
             {'nom':'Lara','prénom':'Croft'},
@@ -13,5 +13,7 @@ let arr2 = [{'nom':'Abdelhadi','prénom':'Karim'},
  console.log(arr1); // affichage attendu: [a, b ,c]
  
  
- //
+ //Ajout ou retrait de données
+
+arr1.push('d');
 
