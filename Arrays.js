8fa728eed6e2693d91977bde7e420ca025e66890
AbdKarim <41269340+Karimfactory@users.noreply.github.com>
2020-02-11 19:23:08 +0100

    Create Arrays.js

---
+++ Arrays.js
@@ -0,0 +1,17 @@
+// création de tableaux
+
+let arr1 = [a, b ,c];
+let arr2 = [{'nom':'Abdelhadi','prénom':'Karim'},
+            {'nom':'John','prénom':'Wick'},
+            {'nom':'Lara','prénom':'Croft'},
+            {'nom':'Harry','prénom':'Potter'}
+            ];
+            
+            
+ //Affichage de données dans la console
+ 
+ console.log(arr1); // affichage attendu: [a, b ,c]
+ 
+ 
+ //
+
